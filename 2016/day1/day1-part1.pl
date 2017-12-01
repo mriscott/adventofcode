9411ab9cc8037a5e3ba6749ccb1efade436a1508
Ian Scott <ian.scott@thomsonreuters.com>
2017-12-01 16:07:57 +0000

    Added day1 2016

---
+++ 2016/day1/day1-part1.pl
@@ -0,0 +1,41 @@
+#!/bin/perl
+
+$n=0;
+$e=0;
+$d=0;
+
+@norths=(1,0,-1,0);
+@easts=(0,1,0,-1);
+@dirs=('N','E','S','W');
+
+
+foreach (@ARGV){
+	($t,$s)=/([RL])(\d+)/;
+	if($t eq 'L'){
+		$d--;
+		$d=3 if ($d==-1);
+	}
+	elsif($t eq 'R'){
+		$d++;
+		$d=0 if ($d==4);
+	}
+	else {
+		print("Error: Bad turn $t\n");
+		exit (0);
+	}
+	print("Turn $t, walk $s blocks $dirs[$d]\n");
+	for ($i=0;$i<$s;$i++){
+		$n+=($norths[$d]);
+		$e+=($easts[$d]);
+		$loc="$n,$e";
+		print "$loc\n";
+	}
+	last if $break;
+}
+
+print "$n blocks north, $e blocks east\n";
+$n=-$n if $n<0;
+$e=-$e if $e<0;
+print "You are ".($n+$e)." blocks away\n";
+	
+
