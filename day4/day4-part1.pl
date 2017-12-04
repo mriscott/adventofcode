a4d2d7104f5e4b91f32b71f4319be247bb40968a
Ian Scott <ian.scott@thomsonreuters.com>
2017-12-04 11:02:56 +0000

    Day4

---
+++ day4/day4-part1.pl
@@ -0,0 +1,19 @@
+$total=0;
+foreach(<STDIN>){
+	chomp;
+	print;
+	@words=split / /;
+	%hashwords={};
+	$valid=1;
+	foreach $word (@words){
+		if($hashwords{$word}){
+			print " - $word";
+			$valid=0;
+		}
+		$hashwords{$word}=$word;
+	}
+	print(":$valid\n");
+	$total+=$valid;
+		
+}
+print "\n$total valid\n";
