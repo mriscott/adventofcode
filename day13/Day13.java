df8f25fe1b595b70c2863aa0ca17cb191322b015
Ian Scott <ian.scott@thomsonreuters.com>
2017-12-13 16:32:08 +0000

    Day13 - complete

--- day13/Day13.java
+++ day13/Day13.java
@@ -5,20 +5,42 @@ public class Day13{
     Layer[] levels;
     
     public static void main(String [] args){
+	System.out.println("Testing example");
 	Day13 example=new Day13(true);
-	int severity = example.cross();
+	int severity = example.cross(0,false);
 	if(severity!=24){
 	    System.out.println("Severity should be 24 - found "+severity);
 	    System.exit(1);
 	}
+
+
+	int delay = example.getDelay();
+	
+	if(delay!=10){
+	    System.out.println("Delay should be 10 - found "+delay);
+	    System.exit(1);
+	}
 	Day13 real=new Day13(false);
-        System.out.println("Real severity:"+real.cross());
+        System.out.println("Real severity:"+real.cross(0,false));
+	
+        System.out.println("Real delay:"+real.getDelay());
     }
 
     public Day13(boolean test){
 	if(test) setUpExample();
 	else setUpData();
     }
+
+    public int getDelay(){
+	int delay=0;
+	while(cross(delay,true)!=-1) {
+	    //System.out.println("Caught after "+delay+" ps wait");
+	    delay++;
+	}
+	    
+	return delay;
+    }
+    
 
     public void setUpExample(){
 	levels=new Layer[7];
@@ -75,34 +97,78 @@ public class Day13{
 		levels[88] = new Layer( 14);
     }
 
-    int cross(){
+    int cross(int wait, boolean shortcut){
+	//	System.out.println("\n\nDelay:"+wait);
+	//if(wait%1000==0) System.out.println(wait);
+	reset();
+	moveOn(wait);
+	//for(int x=0;x<wait;x++) moveAll();
 	int severity=0;
+	boolean caught=false;
 	for(int x=0;x<levels.length;x++){
-	    System.out.println("Level:"+x);
-	    severity+=calcSeverity(x);
+	    //printAll(x);
+	    if(shortcut){
+		if(isCaught(x)){
+		    caught=true;
+		    break;
+		}
+	    }
+	    if(isCaught(x)){
+		severity+=calcSeverity(x);
+		caught=true;
+	    }
 	    moveAll();	    
 	}
+	if(caught==false) return -1;
 	return severity;
     }
 
+    void printAll(int x){
+	System.out.println("After "+x+":");
+	for(int y=0;y<levels.length;y++){
+	    System.out.print(y+")");
+	    if(levels[y]!=null) levels[y].debugPrint(x==y?0:-1);
+	    else if(y==x) System.out.print("x");
+	    System.out.println("");
+			     
+	}
+    }
+
+    void moveOn(int wait){
+	for(int x=0;x<levels.length;x++){
+	    if(levels[x]!=null) {
+		levels[x].moveOn(wait);
+	    }	    
+	}
+    }
+    
     void moveAll(){
 	for(int x=0;x<levels.length;x++){
 	    if(levels[x]!=null) {
 		levels[x].move();
-		//System.out.println("layer "+x+" - pos "+levels[x].getPos());
+	    }	    
+	}
+    }
+
+    void reset(){
+	for(int x=0;x<levels.length;x++){
+	    if(levels[x]!=null) {
+		levels[x].reset();
 	    }
 	}
     }
 
     int calcSeverity(int x){
+	if(!isCaught(x)) return 0;
+	return x*levels[x].getDepth();
+    }
+
+    boolean isCaught(int x){
 	Layer level=levels[x];
-	if(level==null) return 0;
-	if(level.getPos()!=0) return 0;
-	int sev=x*level.getDepth();
-	System.out.println("Caught in level "+x+":"+sev);
-	return sev;
-	
-	
+	if(level==null) return false;
+	if(level.getPos()!=0) return false;
+	//System.out.println("Caught in level "+x);
+	return true;
     }
 	
 
@@ -114,6 +180,10 @@ public class Day13{
 	
 	public Layer(int size){
 	    depth=size;
+	    reset();
+	}
+
+	public void reset(){
 	    pos=0;
 	    direction=+1;
 	}
@@ -131,6 +201,23 @@ public class Day13{
 	    if(pos==0) direction=1;
 	    if(pos==(depth-1)) direction=-1;
 	}
+
+	public void moveOn(int x){
+	    if(pos!=0) throw new IllegalStateException("Cant run moveon when not at start");
+	    int steps = x%((depth*2)-2);
+	    for(int y=0;y<steps;y++) move();
+	}
+
+	
+
+	public void debugPrint(int x){	    
+	    for(int y=0;y<depth;y++){
+		if(y==pos && y==x) System.out.print("*");
+		else if(y==pos) System.out.print("s");
+		else System.out.print((x==0&&y==0)?"x":".");
+	    }
+	}
+	    
     }
 }
     
