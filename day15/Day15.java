7a03e840800dde22b1e1be708971954f36a87e08
Ian Scott <ian.scott@thomsonreuters.com>
2017-12-18 12:47:34 +0000

    Day15 complete

--- day15/Day15.java
+++ day15/Day15.java
@@ -5,18 +5,20 @@ public class Day15{
     Generator genb;
 
     public Day15(long starta, long startb){
-	gena=new Generator(starta,16807);
-	genb=new Generator(startb,48271);
+	gena=new Generator(starta,16807,4);
+	genb=new Generator(startb,48271,8);
     }
 
     public int iterate(int num){
-	if(num==0) num=40000000;
+	if(num==0) num=5000000;
 	int match=0;
-	//System.out.println("--Gen, A--\t--Gen. B--");
+	boolean debug=false;
+	if(debug) System.out.println("--Gen, A--\t--Gen. B--");
 	for(int x=0;x<num;x++){
 	    if(x%1000==0) System.out.print("\rIteration "+x+"/"+num);
 	    long aval=gena.generateValue() & 0xffff;
 	    long bval=genb.generateValue() & 0xffff;
+	    if(debug) System.out.println(gena.getValue()+"\t"+genb.getValue());
 	    if(aval==bval) match++;
 	}
 	System.out.println("");
@@ -37,10 +39,12 @@ public class Day15{
 
     
     public static void  main(String [] args){
-	if(!test("short run",65,8921,5,1)) return;
-	if(!test("long run", 65,8921,0,588)) return;
+	if(!test("short run",65,8921,5,0)) return;
+	if(!test("long run", 65,8921,1055,0)) return;
+	if(!test("first match", 65,8921,1056,1)) return;
+	if(!test("full run", 65,8921,0,309)) return;
 	Day15 real = new Day15(783,325);
-	int matches=real.iterate(40000000);
+	int matches=real.iterate(0);
 	System.out.println("Matches:"+matches);
     }
 }
@@ -49,10 +53,12 @@ class Generator{
     long value;
     long factor;
     static final long divisor=2147483647;
+    int verify;
     
-    public Generator(long val, long fac){
+    public Generator(long val, long fac, int v){
 	value=val;
 	factor=fac;
+	verify=v;
     }
 
     public long getValue(){
@@ -62,6 +68,7 @@ class Generator{
     public long generateValue(){
 	long newval=value*factor;
 	value=newval % divisor;
+	if(value % verify!=0) value=generateValue();
 	return value;
     }
 
