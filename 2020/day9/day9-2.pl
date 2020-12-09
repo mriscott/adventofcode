72e397dde97b7df2ba770d87dc868b2929cc159c
root <root@iPhone.Home>
2020-12-09 22:35:10 +0000

    wip

--- 2020/day9/day9-2.pl
+++ 2020/day9/day9-2.pl
@@ -1,6 +1,6 @@
 @nos=0;
 $i=0;
-$p=5;
+$p=25;
 foreach (<STDIN>){
 chomp;
 $nos[$i]=$_;
@@ -9,6 +9,7 @@ $valid=0;
 for ($x=$i-1;$x>($i-$p-1);$x--){
 for ($y=$i-1;$y>($i-$p-1);$y--){
 if($x!=$y && ($nos[$x]+$nos[$y]==$nos[$i])){
+print("$nos[$x]+$nos[$y]=$nos[$i]\n");
 $valid=1;
 last;
 }
@@ -22,3 +23,21 @@ last;
 }
 $i++;
 }
+$t=$nos[$i];
+for($x=$t-1;$x--;$x>0){
+$tot=0;
+$s=-1;
+$l=-1;
+$y=$x;
+while($tot<$t){
+$tot+=$nos[$y];
+$l=$nos[$y] if($nos[$y]>$l) ;
+$s=$nos[$y] if($nos[$y]<$s) ;
+$s=$nos[$y] if($nos[$y]==-1) ;
+if($tot==$t){
+$m=$l+$s;
+print ("$l, $s $m\n");}
+}
+$y--;
+}
+print ("end\n);
