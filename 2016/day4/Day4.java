dc758a3be836f0aeffcdec08458399d073cb26d0
Ian Scott <mriscott@yahoo.co.uk>
2022-10-22 14:53:15 +0100

    Start on day 4

---
+++ 2016/day4/Day4.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class Day4{
+
+	public static void main(String [] args){
+		try{
+			Day4 test=new Day4();
+			System.out.println("Test:"+test.test());
+			Day4 test2=new Day4();
+			System.out.println("Test2:"+test.testRead());
+			//Day2 real=new Day2();
+			//System.out.println("Answer:"+real.read(args.length>0?args[0]:"input"));
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	
+
+	}
+	
+	public boolean test()
+	{
+		boolean success=true;
+		if (processLine("aaaaa-bbb-z-y-x-123[abxyz]")!=1) success=false;
+		if(processLine("a-b-c-d-e-f-g-h-987[abcde]")!=1) success=false;
+		if(processLine("not-a-real-room-404[oarel]")!=1) success=false;
+		if(processLine("totally-real-room-200[decoy]")!=0) success=false;;
+		return(success); 
+		
+	}
+
+	public boolean testRead() throws IOException{
+		int count=read("test");
+		return(count==3);
+	}
+
+	public int read(String filename) throws IOException {
+		System.out.println("Reading "+filename);
+		LineNumberReader fr=new LineNumberReader(new FileReader(new File(filename)));
+		String line=fr.readLine();
+		int count=0;
+		while(line!=null){
+			count+=processLine(line);
+			line=fr.readLine();
+		}
+		fr.close();
+		return count;
+	}
+
+	
+
+	public Day4(){
+	}
+
+
+	// return 1 if this is a real room, 0 otherwise
+	public int  processLine(String line){
+		boolean data=true;
+		boolean cksum=false;
+		String checksum="";
+		String roomno="";
+		Hashtable counts=new Hashtable();
+		for(int i=0;i<line.length();i++){
+			char c=line.charAt(i);
+			if (data && c>='a'&& c<='z'){
+				int count=0;
+				if(counts.get(c)!=null) {
+				       	count=Integer.parseInt(counts.get(c).toString());
+				}
+
+				count++;
+				counts.put(c,""+count);
+			}
+			if(c>='0' && c<='9'){
+				// room number
+				data=false;
+				roomno+=c;
+			}
+			if(c=='['){
+				cksum=true;
+			}
+			
+			if (cksum && c>='a'&& c<='z'){
+				checksum+=c;
+			}
+
+		}
+		System.out.println("Room : "+roomno+" Checksum:"+checksum);
+		return 0;
+	}
+
+}
