229385f34a207821f52067bd4ae8ce7999816c99
irongeek <mriscott@yahoo,co,uk>
2022-11-01 03:35:12 +0000

    Day9 Part 1

--- 2016/day9/Day9.java
+++ 2016/day9/Day9.java
@@ -1,5 +1,7 @@
+import java.io.*;
 public class Day9
 {
+	int len=0;
 	public Day9(){
 	}
 
@@ -15,11 +17,32 @@ public class Day9
 				String rep=input.substring(paren+1,paren+1+range);
 				x=paren+range;
 				for(int i=0;i<count;i++) output.append(rep);
+				len+=count*range;
 						
 			}
-			else output.append(ch);
+			else {
+				output.append(ch);
+				if(!Character.isWhitespace(ch)){
+					len++;
+				}
+			}
 		}
 		return output.toString();
+	}
+
+	public void read(String filename) throws IOException {
+		System.out.println("Reading "+filename);
+		LineNumberReader fr=new LineNumberReader(new FileReader(new File(filename)));
+		String line=fr.readLine();
+		while(line!=null){
+			decompress(line);
+			line=fr.readLine();
+		}
+		fr.close();
+	}
+
+	public int getDecompressedLength(){
+		return len;
 	}
 
 	public static void main(String [] args){
@@ -30,13 +53,22 @@ public class Day9
 		test.testDecompress("A(2x2)BCD(2x2)EFG","ABCBCDEFEFG");
 		test.testDecompress("(6x1)(1x3)A","(1x3)A");
 		test.testDecompress("X(8x2)(3x3)ABCY","X(3x3)ABC(3x3)ABCY");
+		test.testDecompressedLength(6+7+9+11+6+18);
 		System.out.println("All tests passed");
+		Day9 part1=new Day9();
+		try{
+			part1.read("input.txt");
+			System.out.println("Decompressed length : "+part1.getDecompressedLength());
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 	public void testDecompress(String input,String output){
 		String real=decompress(input);
 		if(real.equals(output)){
 			System.out.println("Test: "+input+" -> "+output+" :PASS");
+			System.out.println("len:"+len);
 		}
 		else{
 
@@ -44,6 +76,15 @@ public class Day9
 
 		}
 
+	}
+
+	public void testDecompressedLength(int expected){
+		if(expected==getDecompressedLength()){
+			System.out.println("Length test passed");
+		}
+		else{
+			throw new RuntimeException("Length is "+getDecompressedLength()+" - expected "+expected);
+		}
 	}
 
 
