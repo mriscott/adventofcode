0195d64b4c4c16380eed4c5410e7fed05da49937
irongeek <mriscott@yahoo,co,uk>
2022-11-02 04:42:36 +0000

    Split out expansion - still doesn;t work completelty

--- 2016/day9/Day9.java
+++ 2016/day9/Day9.java
@@ -2,8 +2,66 @@ import java.io.*;
 public class Day9
 {
 	int len=0;
-	int len2=0;
-	public Day9(){
+	int part;
+	public Day9(int p){
+		part=p;
+	}
+
+	class Chunk{
+		String chunk;
+		String rep;
+		int count;
+		int range;
+
+		Chunk(int x, String input){
+			int sep=input.indexOf("x",x);
+			range=Integer.parseInt(input.substring(x+1,sep));
+			int paren=input.indexOf(")",sep);
+			count=Integer.parseInt(input.substring(sep+1,paren));
+			rep=input.substring(paren+1,paren+1+range);
+			chunk=input.substring(x,paren+1);
+		}
+		public String toString(){
+			return "Chunk:"+chunk+" ["+range+","+count+"]";
+		}
+
+		void expand(boolean recurse){
+			StringBuffer expandedrep=new StringBuffer();
+			for(int x=0;x<rep.length();x++){
+				if(recurse && rep.charAt(x)=='('){
+					Chunk sub=new Chunk(x,rep);
+					x=movePastChunk(x);
+					sub.expand(true);
+					expandedrep.append(sub.rep);
+				}
+				else{
+					expandedrep.append(rep.charAt(x));
+				}
+			}
+			StringBuffer exp=new StringBuffer();
+			for(int i=0;i<count;i++) {
+				exp.append(expandedrep);
+			}
+			rep=exp.toString();
+
+		}
+
+		int movePastChunk(int x){
+			x+=chunk.length();
+			x+=range;
+			x--;
+			return x;
+		}
+	}
+
+
+	int decompressChunk(int x,String input,StringBuffer output){
+		Chunk chunk=new Chunk(x,input);
+		x=chunk.movePastChunk(x);	
+		chunk.expand(part==2);
+		output.append(chunk.rep);
+		len+=chunk.count*chunk.range;
+		return x;
 	}
 
 	public String decompress(String input){
@@ -11,15 +69,7 @@ public class Day9
 		for(int x=0;x<input.length();x++){
 			char ch=input.charAt(x);
 			if(ch=='('){
-				int sep=input.indexOf("x",x);
-				int range=Integer.parseInt(input.substring(x+1,sep));
-				int paren=input.indexOf(")",sep);
-				int count=Integer.parseInt(input.substring(sep+1,paren));
-				String rep=input.substring(paren+1,paren+1+range);
-				x=paren+range;
-				for(int i=0;i<count;i++) output.append(rep);
-				len+=count*range;
-						
+				x=decompressChunk(x,input,output);
 			}
 			else {
 				output.append(ch);
@@ -32,7 +82,7 @@ public class Day9
 	}
 
 	public int decompress2Len(String input){
-		return calcLen(decompress2(input));
+		return calcLen(decompress(input));
 	}
 
 	int calcLen(String input){
@@ -43,22 +93,14 @@ public class Day9
 		return l;
 	}
 
-	public String decompress2(String input){
-		String output=input;
-		while(output.indexOf("(")!=-1){
-			output=decompress(output);
-		}
-		return output;
-	}
 
 
-	public void read(String filename,int part) throws IOException {
+	public void read(String filename) throws IOException {
 		System.out.println("Reading "+filename);
 		LineNumberReader fr=new LineNumberReader(new FileReader(new File(filename)));
 		String line=fr.readLine();
 		while(line!=null){
-			if(part==1) decompress(line);
-			if(part==2) decompress2(line);
+			decompress(line);
 			line=fr.readLine();
 		}
 		fr.close();
@@ -69,7 +111,7 @@ public class Day9
 	}
 
 	public static void main(String [] args){
-		Day9 test = new Day9();
+		Day9 test = new Day9(1);
 		test.testDecompress("ADVENT","ADVENT");
 		test.testDecompress("A(1x5)BC","ABBBBBC");
 		test.testDecompress("(3x3)XYZ","XYZXYZXYZ");
@@ -78,28 +120,31 @@ public class Day9
 		test.testDecompress("X(8x2)(3x3)ABCY","X(3x3)ABC(3x3)ABCY");
 		test.testDecompressedLength(6+7+9+11+6+18);
 		System.out.println("Part 1 tests passed");
-		Day9 part1=new Day9();
+		Day9 part1=new Day9(1);
 		try{
-			part1.read("input.txt",1);
+			part1.read("input.txt");
 			System.out.println("Decompressed length : "+part1.getDecompressedLength());
 		}catch(IOException e){
 			e.printStackTrace();
 			return;
 		}
-		Day9 test2= new Day9();
+		Day9 test2= new Day9(2);
 		test2.testDecompress2Len("(3x3)XYZ",9);
 		test2.testDecompress("(3x3)XYZ","XYZXYZXYZ",2);
 		test2.testDecompress("X(8x2)(3x3)ABCY","XABCABCABCABCABCABCY",2);
 		test2.testDecompress2Len("(27x12)(20x12)(13x14)(7x10)(1x12)A",241920);
 		test2.testDecompress2Len("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN",445);
+		System.out.println("Part 2 tests passed");
+		/*
 		try{
-			Day9 part2=new Day9();
-			part2.read("input.txt",2);
+			Day9 part2=new Day9(2);
+			part2.read("input.txt");
 			System.out.println("Decompressed length : "+part2.getDecompressedLength());
 		}catch(IOException e){
 			e.printStackTrace();
 			return;
 		}
+		*/
 	}
 
 	public void testDecompress2Len(String input,int len){
@@ -121,8 +166,7 @@ public class Day9
 
 	public void testDecompress(String input,String output,int part){
 		String real=null;
-		if(part==1) real=decompress(input);
-		else real=decompress2(input);
+		real=decompress(input);
 		if(real.equals(output)){
 			System.out.println("Test: "+input+" -> "+output+" :PASS");
 			System.out.println("len:"+len);
