d028a3d51c5497b94fbd326d989a1d1323a4f8ef
Ian Scott <Ian Scott>
2024-12-02 16:38:23 +0000

    day2pt2-eventually

--- 2024/day2.py
+++ 2024/day2.py
@@ -1,21 +1,46 @@
-def issafe(line):
-	arr=x.split()
+def issafe(arr,damp):
 	dir=0
+		
 	for i in range(len(arr)-1):
 		d=int(arr[i+1])-int(arr[i])
+		ok=1
 		if((dir<0 and d>0) or (dir>0 and d<0) or d==0):
-			return 0
+			ok=0
 		if(abs(d)>3):
-			return 0
+			ok=0 
 		dir=d
-	return 1
+		if(ok==0):
+			if(damp==0):
+				for j in (0,i,i+1):
+					damp=1	
+					arr2=arr.copy()
+					arr2.pop(j)
+					if(issafe(arr2,damp)==1) :
+						ok=1
+						break
+				if(ok==0):
+					break
+				else:
+					break
+			break
+	return ok
 
 	
 
 f=open("input.txt","r")
-safe=0
+#f=open("test.txt","r")
+safe1=0
+safe2=0
 for x in f:
-	safe+=issafe(x)
+	safe1+=issafe(x.split(),1)
+	safe2+=issafe(x.split(),0)
 
 f.close()
-print ("Pt1: "+str(safe))
+print()
+print()
+print()
+print()
+print ("Pt1: "+str(safe1))
+print ("Pt2: "+str(safe2))
+
+#too low 564
