f30ee98bf66d35223ba53fbb1996f5eb1b1c1c95
Ian Scott <Ian Scott>
2024-12-04 18:33:44 +0000

    d4p2

--- 2024/day4.py
+++ 2024/day4.py
@@ -9,7 +9,6 @@ if(len(sys.argv)>1):
 	for l in  f:
 		grid.append(l.strip())
 	f.close()
-	print (grid)
 
 
 def charat(x,y):
@@ -43,14 +42,26 @@ def search(x,y):
 				m+=1	
 	return m
 
+
+def searchx(x,y):
+	a1=charat(x-1,y-1)+charat(x+1,y+1)
+	a2=charat(x-1,y+1)+charat(x+1,y-1)
+	if((a1=="SM" or a1=="MS") and (a2=="SM" or a2=="MS")):
+		return 1
+	return 0
+
 pt1=0
+pt2=0
 for y in range(len(grid)):
 	s=grid[y]
 	for x in range(len(s)):
 		if(s[x]==target[0]):
 			pt1+=search(x,y)
+		if(s[x]=='A'):
+			pt2+=searchx(x,y)
 	
 
 
 print ("Part 1: ",str(pt1))
+print ("Part 2: ",str(pt2))
 
