277c0420afdc8bdae15ac52749e45987e7847f30
Ian Scott <Ian Scott>
2024-12-04 17:17:23 +0000

    d4p1

---
+++ 2024/day4.py
@@ -0,0 +1,56 @@
+import sys
+
+target="XMAS"
+grid=[]
+if(len(sys.argv)>1):
+	inp=""
+
+	f=open(sys.argv[1],"r")
+	for l in  f:
+		grid.append(l.strip())
+	f.close()
+	print (grid)
+
+
+def charat(x,y):
+	if(x<0 or y<0 or y>=len(grid)):
+		return '.'
+	s=grid[y]
+	if(x>=len(s)):
+		return '.'
+	return s[x]
+
+
+def checkWord(x,y,dx,dy,c):
+	x+=dx
+	y+=dy
+	c+=1
+	if(c==len(target)):
+		return True
+	if(charat(x,y)==target[c]):
+		return checkWord(x,y,dx,dy,c)
+	return False
+
+
+# return no of words found starting at x,y
+def search(x,y):
+	m=0
+	if(charat(x,y)!=target[0]):
+		 return False
+	for dx in (1,0,-1):
+		for dy in (1,0,-1):
+			if(checkWord(x,y,dx,dy,0) ):
+				m+=1	
+	return m
+
+pt1=0
+for y in range(len(grid)):
+	s=grid[y]
+	for x in range(len(s)):
+		if(s[x]==target[0]):
+			pt1+=search(x,y)
+	
+
+
+print ("Part 1: ",str(pt1))
+
