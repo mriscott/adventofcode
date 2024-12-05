dc21c86fd49cb600ba5ec35ff568cc91d461fa87
Ian Scott <Ian Scott>
2024-12-05 18:51:18 +0000

    d5p2

--- 2024/day5.py
+++ 2024/day5.py
@@ -17,19 +17,30 @@ if(len(sys.argv)>1):
 
 
 pt1=0
+pt2=0
 
 for p in pages:
 	ok=True
-	for r in rules:
-		if (r[0] in p and r[1] in p):
-			if(p.index(r[0])>p.index(r[1])):
-				ok=False				
+	ordered=False
+	while (ordered==False):
+		ordered=True
+		for r in rules:
+			if (r[0] in p and r[1] in p):
+				if(p.index(r[0])>p.index(r[1])):
+					ok=False
+					ordered=False
+					i=p.index(r[0])
+					ii=p.index(r[1])
+					p[i]=r[1]
+					p[ii]=r[0]
 
+	mid=int((len(p)-1)/2)
 	if(ok):
-		mid=int((len(p)-1)/2)
 		pt1+=int(p[mid])
-
+	else:
+		pt2+=int(p[mid])
 
 
 print ("Part 1:",int(pt1))
+print ("Part 2:",int(pt2))
 			
