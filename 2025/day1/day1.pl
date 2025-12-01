5c74de9b17c23a7bf62ba2942ad19dea666bf990
Ian Scott <Ian Scott>
2025-12-01 21:10:46 +0000

    d1pt2

--- 2025/day1/day1.pl
+++ 2025/day1/day1.pl
@@ -1,13 +1,22 @@
-$tot=0;
+$pt1=0;
+$pt2=0;
 $pos=50;
 foreach (<STDIN>){
 	chomp;
 	s/R/+/;
 	s/L/-/;
-	$_=$_%100;
+	$_=$_;
+	next if ($_==0);
+	$oldpos=$pos;
 	$pos+=$_;
-	$pos-=100 if ($pos>99);
-	$pos+=100 if ($pos<0);
-	$tot++ if ($pos==0);
+	$pt1++ if($pos%100 ==0);
+	$pt2-- if($pos%100 ==0);
+	while ($pos<0 || $pos>99){
+		$pos-=100 if ($pos>99);
+		$pos+=100 if ($pos<0);
+		$pt2++;
+	}
 }
-print ($tot);
+$pt2+=$pt1;
+print ("Part 1: $pt1\n");
+print ("Part 2: $pt2\n");
