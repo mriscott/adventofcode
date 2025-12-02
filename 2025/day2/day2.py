8b29b901eb9b57d843b99488543fea2ed84fab0a
Ian Scott <Ian Scott>
2025-12-02 17:00:09 +0000

    d2p1

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -3,24 +3,16 @@ pt1=0
 
 def checkNum(num):
 	snum=str(num)
-	for l in range(1,int(len(snum)/2)+1):
-		if(len(snum)%l !=0):
-			continue
-		invalid=True
-		slice=snum[:l]
-		for y in range(l,len(snum),l):
-			bit=snum[y:y+l]
-			if(bit!=slice):	
-				invalid=False
-				break
-		if(invalid):
-			print(snum," -Repeat:",slice)
-			return False
-	return True	
+	l=len(snum)
+	mid=int(l/2)
+	if (l%2==1):
+		return True
+	if(snum[:mid] == snum[mid:]):
+		return False
+	return True
 
 for line in fileinput.input():
 	ranges=line.rstrip().split(",")
-	print(ranges)
 	for therange in ranges:
 		ends=therange.split("-")
 		for x in range(int(ends[0]),(int(ends[1])+1)):
