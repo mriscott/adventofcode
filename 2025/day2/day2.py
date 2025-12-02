498c172c4a118659f46fda83a682945c88103f4c
Ian Scott <Ian Scott>
2025-12-02 17:06:02 +0000

    d2p2

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -1,5 +1,6 @@
 import fileinput
 pt1=0
+pt2=0
 
 def checkNum(num):
 	snum=str(num)
@@ -11,6 +12,23 @@ def checkNum(num):
 		return False
 	return True
 
+def checkNum2(num):
+	snum=str(num)
+	for l in range(1,int(len(snum)/2)+1):
+		if(len(snum)%l !=0):
+			continue
+		invalid=True
+		slice=snum[:l]
+		for y in range(l,len(snum),l):
+			bit=snum[y:y+l]
+			if(bit!=slice):
+				invalid=False
+				break
+		if(invalid):           
+			return False
+	return True
+
+
 for line in fileinput.input():
 	ranges=line.rstrip().split(",")
 	for therange in ranges:
@@ -18,4 +36,7 @@ for line in fileinput.input():
 		for x in range(int(ends[0]),(int(ends[1])+1)):
 			if(checkNum(x)==False):
 				pt1=pt1+x
+			if(checkNum2(x)==False):
+				pt2=pt2+x
 print (f"Pt1: {pt1}")
+print (f"Pt2: {pt2}")
